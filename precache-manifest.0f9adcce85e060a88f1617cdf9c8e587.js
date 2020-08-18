05c980c5a2f0f3acf1c95c6bb709810335322403
Lisa Miller <lisayevonne@outlook.com>
2020-08-18 14:21:34 -0400

    Updates

---
+++ precache-manifest.0f9adcce85e060a88f1617cdf9c8e587.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "833958e77ea3705d6f4f0e14ac8bceda",
+    "url": "/frontend-personality-quiz/index.html"
+  },
+  {
+    "revision": "3033fd5347399586293c",
+    "url": "/frontend-personality-quiz/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "471c9eca5fd46ce7e5c3",
+    "url": "/frontend-personality-quiz/static/js/2.57b0bdfb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/frontend-personality-quiz/static/js/2.57b0bdfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3033fd5347399586293c",
+    "url": "/frontend-personality-quiz/static/js/main.f2e97b55.chunk.js"
+  },
+  {
+    "revision": "f1b093936b570657a860",
+    "url": "/frontend-personality-quiz/static/js/runtime-main.7e5b25fc.js"
+  },
+  {
+    "revision": "80747659efac5bc768915c43225de3ef",
+    "url": "/frontend-personality-quiz/static/media/george.80747659.png"
+  },
+  {
+    "revision": "73bb2d5208af0f0ebf3144bd87138943",
+    "url": "/frontend-personality-quiz/static/media/kaley.73bb2d52.png"
+  },
+  {
+    "revision": "f3f81c9cb2a321efd41bc270a2dbbbfb",
+    "url": "/frontend-personality-quiz/static/media/lionel.f3f81c9c.png"
+  },
+  {
+    "revision": "f974c3ce42b7ec871ec688947394ef25",
+    "url": "/frontend-personality-quiz/static/media/pasha.f974c3ce.png"
+  },
+  {
+    "revision": "f44cf37e309ffefe5f4444cbfd4ce5a3",
+    "url": "/frontend-personality-quiz/static/media/zach.f44cf37e.png"
+  }
+]);
