fb86278e86f382a7bbb7c3c11b82e6e4cb5f014b
Lisa Miller <lisayevonne@outlook.com>
2020-08-19 10:00:54 -0400

    user

--- src/components/UserComponent.js
+++ src/components/UserComponent.js
@@ -3,8 +3,7 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 import { Grid, TextField, Button } from "@material-ui/core";
 import { AccountCircleRounded } from "@material-ui/icons";
-import UserService from "../services/UserService";
-import { Typography, Button, Grid, TextField } from "@material-ui/core";
+import { Button, Grid, TextField } from "@material-ui/core";
 
 class UserComponent extends Component {
   state = {};
@@ -15,89 +14,62 @@ class UserComponent extends Component {
       name: "",
     };
 
-    // this.userChange = this.userChange.bind(this);
-    // this.handleSubmit = this.handleSubmit.bind(this);
-
-    // this.submitUser = this.submitUser.bind(this);
+    this.userChange = this.userChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  //   userChange = (event) => {
-  //     event.preventDefault();
-  //     this.setState({ name: event.target.value });
-  //     console.log("name:" + this.state.name);
-  //   };
+  userChange = (event) => {
+    event.preventDefault();
+    this.setState({ name: event.target.value });
+    console.log("name:" + this.state.name);
+  };
 
-  //   handleSubmit(event) {
-  //     event.preventDefault();
-  //     alert(this.state.name);
-  //     this.submitUser();
-  //   }
-  //   componentDidMount() {
-  //     if (this.props.name) {
-  //       this.setState({
-  //         name: this.props.name,
-  //       });
-  //     }
-  //   }
-  //   saveUser = (event) => {
-  //     this.setState({ name: event.target.value });
-  //   };
+  handleSubmit(event) {
+    event.preventDefault();
+    alert(this.state.name);
+    this.submitUser();
+  }
 
-  //   submitUser = () => {
-  //     event.preventDefault();
-  //     const user = { userName: this.state.name };
-  //     axios.post("http://localhost:8080/user/names", user);
-  //      let name = this.state;
-  //     UserService.createUserNames(name).then((response) => {
-  //       this.props.history.push("names");
-  //     });
-  //   };
+  saveUser = (event) => {
+    this.setState({ name: event.target.value });
+  };
 
-  // using this yt video https://www.youtube.com/watch?v=ieMhlyjPjWo
-
-  // userList = () => {
-  //   return this.props.history.push("names");
-  // };
-
-  // handleSubmit = (event) => {
-  //   event.preventDefault();
-  //   let name = this.state.name;
-  //   UserService.createUserNames(name).then((response) => {
-  //     this.props.history.push("addUser");
-  //   });
-  //   this.setState({ name: "" });
-  //   console.log(this.state.name);
-  // };
+  submitUser = () => {
+    const user = { userName: this.state.name };
+    axios.post("https://personality-quiz-wwc.herokuapp.com/user/names", user);
+  };
 
   render() {
     return (
       <div>
-        {/* <Grid item>
-          <TextField
-            id="input-with-icon-grid"
-            label="Username"
-            onChange={this.saveUser}
-          />
-          <Button onClick={this.submitUser}></Button>
-        </Grid> */}
-        {/* <Grid item> */}
-        <TextField
-          id="input-with-icon-grid"
-          label="Username"
-          onChange={this.saveUser}
-        />
-        {/* <Button onClick={this.submitUser}></Button> */}
-        {/* </Grid> */}
+        <form>
+          <Grid container spacing={1} alignItems="flex-end">
+            <Grid item>
+              <AccountCircleRounded />
+            </Grid>
 
-        <Button
-          variant="contained"
-          color="primary"
-          component={Link}
-          to="/quiz"
-          onClick={this.submitUser}
-        >
-          Take Quiz
-        </Button>
+            <Grid item>
+              <TextField
+                id="input-with-icon-grid"
+                label="Username"
+                value={this.state.name}
+                onChange={this.userChange}
+              />
+            </Grid>
+            <Grid item>
+              <Button
+                variant="contained"
+                color="primary"
+                size="small"
+                onClick={this.handleSubmit}
+                component={Link}
+                to="/quiz"
+              >
+                Take Quiz
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
       </div>
     );
   }
